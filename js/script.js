2f588d5725a5a28b5c6d82169a62b90d880ffd09
nazneen636 <nazneennahar636@gmail.com>
2024-09-20 23:15:22 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1000,
+    arrows:false,
+    dots:true,
+    
+  });
+
+$('.slider2').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1000,
+    arrows:false,
+    dots:true,
+  });
